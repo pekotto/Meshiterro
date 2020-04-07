dc0ac05b80c8a6e30fb28c6b05c879bf229e6a5a
vagrant <vagrant@localhost.localdomain>
2020-04-07 07:53:11 +0000

    [5-6]start

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -21,6 +21,11 @@ class PostImagesController < ApplicationController
     	@post_image = PostImage.find(params[:id])
     	@post_comment = PostComment.new
     end
+    def destroy
+        @post_image = PostImage.find(params[:id])
+        @post_image.destroy
+        redirect_to post_images_path
+    end
 end
 
 # 投稿データのストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   # ログイン後、自動で投稿一覧ページへルーティング
   root 'post_images#index'
   # 新規投稿、一覧、詳細機能にのみルーティング
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   # コメント機能はpost_imageと親子関係（親：post_image, 子：comment） なので、post_imagesのresourcesボックスに入れる
     resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
