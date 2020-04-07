2ef9944578f981112a8498c3c573687b1f2974b7
vagrant <vagrant@localhost.localdomain>
2020-04-07 07:30:45 +0000

    [5-5]start

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+
+  devise_for :users
+  # ログイン後、自動で投稿一覧ページへルーティング
+  root 'post_images#index'
+  # 新規投稿、一覧、詳細機能にのみルーティング
+  resources :post_images, only: [:new, :create, :index, :show] do
+  # コメント機能はpost_imageと親子関係（親：post_image, 子：comment） なので、post_imagesのresourcesボックスに入れる
+    resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
